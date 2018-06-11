0a40dc09cd31bdf27965140734b1c145435576a1
Neil Hankey <hankey.neil@gmail.com>
2018-06-11 20:54:35 +0100

    Add initial php files

---
+++ liars-dice/php/dicey-test-main.php
@@ -0,0 +1,9 @@
+<?php
+
+function runAll() {
+    echo "runAll()";
+}
+
+runAll();
+
+?>
---
+++ liars-dice/php/runAll.bat
@@ -0,0 +1,1 @@
+c:\php\php-7.2.6-nts-Win32-VC15-x64\php.exe  C:\Users\user\Desktop\root\cur\work\training\dicey\liars-dice\php\dicey-test-main.php
