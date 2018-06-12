ef2427e5a5427ee7fe2c9c74c3b7321d529f758b
Neil Hankey <hankey.neil@gmail.com>
2018-06-12 23:09:34 +0100

    Write very first php funcion!

--- liars-dice/php/dicey-test-main.php
+++ liars-dice/php/dicey-test-main.php
@@ -1,7 +1,31 @@
 <?php
 
+define('__ROOT__', dirname(__FILE__)); 
+require_once(__ROOT__.'/unitTester.php'); 
+
+
 function runAll() {
     echo "runAll()";
+
+    $allOkay = FALSE;
+    for ($i = 0; $i < 1; $i++)
+    {
+        if (!PokerDiceHandEqualityOperatorTestPassed()) break;
+        //if (!permutationsCalculatorTestPassed()) break;
+        //if (!handKindsIdentifyTestPassed()) break;
+        //if (!pokerDiceHandStringValidatorTestPassed()) break;
+        //if (!PokerDiceHandGreaterLessThanOperatorsTestPassed()) break;
+        //if (!rerollerFacesRemainderTestPassed()) break;
+        //if (!GameEngineNewGameTestPassed()) break;
+        //if (!GameEngineSampleGameTestPassed()) break;
+        //if (!GameEngineStalemateRoundTestPassed()) break;
+        //if (!GameNotFoundErrorTestPassed()) break;
+        $allOkay = TRUE;
+    }
+    if (!$allOkay)
+        echo "......Fail";
+    else
+        echo "......Pass";
 }
 
 runAll();
---
+++ liars-dice/php/unitTester.php
@@ -0,0 +1,27 @@
+<?php
+
+//define('__ROOT__', dirname(dirname(__FILE__))); 
+//require_once(__ROOT__.'/config.php'); 
+
+
+function PokerDiceHandEqualityOperatorTestPassed()
+{
+
+    return FALSE;
+/*
+            pokerDiceHand pdha = new pokerDiceHand("AAJJ9");
+            pokerDiceHand pdhb = new pokerDiceHand("AJ9AJ");
+            if (pdha != pdhb)
+                return false;
+
+            pokerDiceHand pdhc = new pokerDiceHand("AJAAJ");
+            if (pdha == pdhc)
+                return false;
+
+            return true;
+*/
+
+}
+
+
+?>
